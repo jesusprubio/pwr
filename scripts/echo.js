28b7b53752c5a64d298c4eef912f76866c97084b
Jesús Pérez <jesusprubio@gmail.com>
2019-03-20 18:32:38 +0100

    Finishing last commit

---
+++ scripts/echo.js
@@ -0,0 +1,13 @@
+/**
+ * @license
+ *
+ * Copyright (c) 2019, Jesús Rubio <jesusprubio@member.fsf.org>
+ *
+ * This source code is licensed under the MIT License found in
+ * the LICENSE.txt file in the root directory of this source tree.
+ */
+
+// eslint-disable-next-line no-console
+console.log(
+  '\n🍕  This is going to take a bit, I include a lot of nice tools ... ;)\n\n',
+);
